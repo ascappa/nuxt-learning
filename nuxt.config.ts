5f8b03e041c81945fb430658a698709d365af6f6
ascappa <andreascappaticci24@gmail.com>
2024-04-10 00:23:58 +0200

    Switch to pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  app: {pageTransition: {name: "page", mode: "default"}}
+  app: { pageTransition: { name: "page", mode: "default" } }
 })
